5625e69a6bb2ee094da5c595414e217779bd3387
helalubo <alejandrodemoraiz@gmail.com>
2020-03-30 01:23:06 -0300

    Investigar sobre realizacion de ejercicio 35, ejercicio 36 resuelto

---
+++ guia de ejercicios/ejercicio 35/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Hacer un que imprima los números de 0 al 100, controlando las filas y
+//las columnas de forma tal que se visualicen en pantalla todos a la vez
+
+int main()
+{
+
+
+    return 0;
+}
---
+++ guia de ejercicios/ejercicio 36/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    for(int i = 0; i<= 10; i++){
+
+
+        for(int k = 0; k< 10; k++){
+
+            printf(" %d X %d = %d \t",i,k,i*k);
+
+        }
+
+
+    }
+
+
+    return 0;
+}
