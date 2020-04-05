9a53af7c1ef93ffde41c409b6828410416a34cbb
helalubo <alejandrodemoraiz@gmail.com>
2020-04-05 02:32:10 -0300

    Ejercicio 48, arreglos en libreria utn

---
+++ guia de ejercicios/ejercicio 48/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CANT 100
+
+#include "utn.h"
+#include "validar.h"
+int main()
+{
+ float salarioSemanal[CANT];
+ int legajo[CANT];
+ float tarifaHora[CANT];
+ int horasTrabajadas[CANT];
+
+ int i = 0;
+ char rta;
+
+
+ do{
+
+    rta = verificarSN("Desea ingresar empleado s/n: ");
+
+
+    if(rta == 's'){
+
+    legajo[i] = getInt("\n Ingrese legajo: " );
+    tarifaHora[i] = getFloat("Ingrese tarifa por hora: ");
+    horasTrabajadas[i] = getInt("Ingrese horas trabajadas: ");
+
+    salarioSemanal[i] = (float)((tarifaHora[i] * horasTrabajadas[i]) *5);
+
+    i++;
+
+    }
+
+
+ }while(rta == 's');
+
+
+ printf("\nSueldos de trabajadores: \n");
+
+ for(int k = 0; k<i; k++){
+
+ printf("LEGAJO: %d \t SALARIO SEMANAL %.2f \n",legajo[k],salarioSemanal[k]);
+
+ }
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ guia de ejercicios/ejercicio 48/validar.c
@@ -0,0 +1,124 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "utn.h"
+
+
+
+
+
+char verificarSN(char mensaje[])
+{
+
+    char respuesta;
+
+    respuesta = tolower(getChar(mensaje));
+
+    while(respuesta != 's' && respuesta != 'n')
+    {
+
+        printf("\nError, ingrese respuesta correcta.\n");
+        respuesta = tolower(getChar(mensaje));
+
+    }
+
+    return respuesta;
+
+
+}
+
+
+
+
+
+
+
+
+
+int isEmail (char cadena[]){
+
+    int i = 0;
+    int flag = 0;
+
+
+    if(cadena != NULL)
+    {
+
+        while(cadena[i] != '\0' )
+        {
+
+            if(cadena[i] == '@')
+            {
+
+                flag = 1;
+            }
+
+            i++;
+
+        }
+
+
+
+    }
+    else
+    {
+
+        flag = 0;
+
+
+    }
+
+
+    return flag;
+}
+
+
+
+int isNumber(char cadena[])
+{
+
+    int i = 0;
+    int flag = 0;
+
+
+    if(cadena != NULL)
+    {
+
+        while(cadena[i] != '\0' )
+        {
+
+            if(cadena[i] >= '0' && cadena [i]<='9')
+            {
+
+                flag = 1;
+            }
+	 else{
+                flag = 0;
+                break;
+            }
+
+            i++;
+
+        }
+
+
+
+    }
+    else
+    {
+
+        flag = 0;
+
+
+    }
+
+
+    return flag;
+}
+
+
+
+
+
