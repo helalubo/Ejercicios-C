b7e2b51e0bb7e710f9119ab06a90178f9fc06d03
helalubo <alejandrodemoraiz@gmail.com>
2020-04-05 07:44:20 -0300

    ejercicios hasta el 57

--- guia de ejercicios/ejercicio 57/main.c
+++ guia de ejercicios/ejercicio 57/main.c
@@ -1,8 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "utn.h"
+
 int main()
 {
-    printf("Hello world!\n");
+    int distancia;
+    float minutos;
+    float segundos;
+
+    float tiempo;
+
+
+
+
+
+
+    float promedio = 0;
+
+    distancia = getInt("Ingrese distancia recorrida en Kilometros: ");
+    minutos = getInt("Ingrese minutos de recorrido: ");
+    segundos = getInt("Ingrese segundos de recorrido: ");
+
+
+
+    tiempo = (float)minutos + (segundos/100);
+
+
+    promedio = (distancia /tiempo)*60;
+
+
+    printf("Distancia: %d\n",distancia);
+    printf("Minutos: %.0f\n",minutos);
+    printf("segundos: %.0f\n",segundos);
+    printf("promedio: %.2f\n",promedio);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
---
+++ guia de ejercicios/ejercicio 57/utn.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+
+
+int getInt(char mensaje[])
+{
+
+    int aux;
+
+    printf("%s",mensaje);
+    scanf("%d",&aux);
+
+    return aux;
+
+
+
+}
+
+int isFloat(float number)
+{
+
+    int aux = 0;
+    int flag = 0;
+
+    aux = number;
+
+    if(number - aux != 0)
+    {
+
+        flag = 1;
+
+    }
+
+
+    return flag;
+
+}
+
+
+float getFloat(char mensaje[])
+{
+
+    float aux;
+
+    printf("%s",mensaje);
+    scanf("%f",&aux);
+
+
+
+
+    return aux;
+
+
+
+}
+
+
+
+char getChar(char mensaje[])
+{
+
+
+    char aux;
+
+    printf("%s",mensaje);
+    aux = getche();
+
+    return aux;
+
+
+}
+
+
+
+
+
+
+void getString(char* cadena[], char mensaje[])
+{
+
+
+    char aux[50];
+
+    printf(mensaje);
+    scanf("%s",&aux);
+
+    strcpy(cadena,aux);
+
+
+
+
+
+}
+
